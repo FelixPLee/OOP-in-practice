6b4764d3c0a4ac6ac7b6fb9e19b31522e889d94e
Felix Pinheiro Lee <felixpinheirolee@gmail.com>
2025-06-11 20:58:01 -0300

    adding the map

--- starter/script.js
+++ starter/script.js
@@ -18,8 +18,19 @@ if (navigator.geolocation)
         console.log(position)
         const {latitude} = position.coords
         const {longitude} = position.coords
+        const coords = [latitude, longitude]
         const googleMapsLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
         console.log(googleMapsLink);
+        const map = L.map('map').setView(coords, 14);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('You are here')
+            .openPopup();
     }, function() {
         alert('Could not get your position')
     })
