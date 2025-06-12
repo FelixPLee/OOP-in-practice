0e0a7041d829e20a556f5c942e46d3bc8383b783
FelixPLee <felixpinheirolee@gmail.com>
2025-06-12 08:14:27 -0300

    adding maker on click

--- starter/script.js
+++ starter/script.js
@@ -11,7 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-
+//getting users position
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(
     function(position) {
@@ -20,17 +20,36 @@ if (navigator.geolocation)
         const {longitude} = position.coords
         const coords = [latitude, longitude]
         const googleMapsLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
-
         console.log(googleMapsLink);
+
+        //adding map
         const map = L.map('map').setView(coords, 14);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+        
+        // pin exemple
+        // L.marker(coords).addTo(map)
+        //     .bindPopup('You are here')
+        //     .openPopup();
 
-        L.marker(coords).addTo(map)
-            .bindPopup('You are here')
-            .openPopup();
+        //adding markers 
+        map.on("click", function(mapEvent) {
+            console.log(mapEvent)
+            const {lat, lng} = mapEvent.latlng
+
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth:250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup'
+                }))
+                .setPopupContent("workout")
+                .openPopup();
+        })
     }, function() {
         alert('Could not get your position')
     })
